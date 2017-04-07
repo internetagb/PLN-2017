4b23d15d99e24a36e7df8c383f15306b9b145a91
Alan Bracco <internetagb@gmail.com>
2017-04-07 17:21:07 -0300

    ej01

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -12,23 +12,44 @@ Options:
 from docopt import docopt
 import pickle
 
-from nltk.corpus import gutenberg
+#from nltk.corpus import gutenberg
 
 from languagemodeling.ngram import NGram
 
+from nltk.corpus import PlaintextCorpusReader
+from nltk.tokenize import RegexpTokenizer
 
 if __name__ == '__main__':
-    opts = docopt(__doc__)
+    #opts = docopt(__doc__)
 
-    # load the data
-    sents = gutenberg.sents('austen-emma.txt')
+    pattern = r'''(?ix)    # set flag to allow verbose regexps
+        (?:Inc\.|sra\.|sr\.)
+        | (?:[A-Z]\.)+        # abbreviations, e.g. U.S.A.
+        | \w+(?:-\w+)*        # words with optional internal hyphens
+        | \$?\d+(?:\.\d+)?%?  # currency and percentages, e.g. $12.40, 82%
+        | \.\.\.            # ellipsis
+        | [][.,;"'?():-_`]  # these are separate tokens; includes ], [
+    '''
 
-    # train the model
-    n = int(opts['-n'])
-    model = NGram(n, sents)
+    tokenizer = RegexpTokenizer(pattern)
 
-    # save it
-    filename = opts['-o']
-    f = open(filename, 'wb')
-    pickle.dump(model, f)
-    f.close()
+    path2 = '/home/alangb/Escritorio/PLN-2017/languagemodeling/scripts/'
+    path = '/home/alangb/Escritorio/'
+    corpus = PlaintextCorpusReader(path2, 'corpus.txt', word_tokenizer=tokenizer)
+
+    for sent in corpus.sents():
+        print("\n")
+        print(sent)
+
+    # # load the data
+    # sents = gutenberg.sents('austen-emma.txt')
+
+    # # train the model
+    # n = int(opts['-n'])
+    # model = NGram(n, sents)
+
+    # # save it
+    # filename = opts['-o']
+    # f = open(filename, 'wb')
+    # pickle.dump(model, f)
+    # f.close()
