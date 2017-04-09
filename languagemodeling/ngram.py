69b52bc63abb9fdea6916c7853afbf00487fbc79
Alan Bracco <internetagb@gmail.com>
2017-04-09 10:42:40 -0300

    ej1 y parte de 2 (4 primeros test ok)

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -14,16 +14,57 @@ class NGram(object):
         self.counts = counts = defaultdict(int)
 
         for sent in sents:
+            for _ in range(n-1):
+                sent = ['<s>'] + sent
+            sent = sent + ['</s>']
             for i in range(len(sent) - n + 1):
                 ngram = tuple(sent[i: i + n])
                 counts[ngram] += 1
                 counts[ngram[:-1]] += 1
 
-    def prob(self, token, prev_tokens=None):
+    def count(self, tokens):
+        """Count for an n-gram or (n-1)-gram.
+        tokens -- the n-gram or (n-1)-gram tuple.
+        """
+        return self.counts[tokens]
+
+
+    def cond_prob(self, token, prev_tokens=None):
+        """Conditional probability of a token.
+        token -- the token.
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
         n = self.n
         if not prev_tokens:
             prev_tokens = []
         assert len(prev_tokens) == n - 1
 
         tokens = prev_tokens + [token]
-        return float(self.counts[tuple(tokens)]) / self.counts[tuple(prev_tokens)]
+        return float(self.counts[tuple(tokens)])/self.counts[tuple(prev_tokens)]
+
+
+    def sent_prob(self, sent):
+        """Probability of a sentence. Warning: subject to underflow problems.
+        sent -- the sentence as a list of tokens.
+        """
+        n = self.n
+        l = len(sent)
+        r = []
+
+        for _ in range(n-1):
+            sent = ['<s>'] + sent
+        sent = sent + ['<\s>']
+
+        for i in range(l):
+            if i < n:
+                r += cond_prob(self, sent[i])
+            else:
+                token = sent[i]
+                prev_tokens = sent[i-n+1:i]
+                r += cond_prob(self, token, prev_tokens)
+
+
+    def sent_log_prob(self, sent):
+        """Log-probability of a sentence.
+        sent -- the sentence as a list of tokens.
+        """
