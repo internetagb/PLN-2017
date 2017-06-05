3fa6255f0e498663cc02193945b507966e352b08
Alan Bracco <internetagb@gmail.com>
2017-06-05 19:09:00 -0300

    UPCFG pasa 4 primeros tests. Cumple flake8.

---
+++ parsing/upcfg.py
@@ -0,0 +1,50 @@
+from parsing.util import unlexicalize, lexicalize
+from collections import defaultdict
+from nltk.grammar import ProbabilisticProduction as PP, PCFG, Nonterminal as N
+from parsing.cky_parser import CKYParser
+from parsing.baselines import Flat
+
+
+class UPCFG:
+    """Unlexicalized PCFG.
+    """
+
+    def __init__(self, parsed_sents, start='sentence'):
+        """
+        parsed_sents -- list of training trees.
+        """
+        countsX = defaultdict(int)
+        countsYZ = defaultdict(lambda: defaultdict(int))
+        ut = [unlexicalize(t.copy(deep=True)) for t in parsed_sents]
+        self.prods = prods = []
+
+        # initialize list of productions
+        for t in ut:
+            for p in t.productions():
+                X = p.lhs()
+                YZ = p.rhs()
+                countsX[X] += 1
+                countsYZ[X][YZ] += 1
+        for (x, v) in countsX.items():
+            for key in countsYZ[x].keys():
+                prods.append(PP(x, key, prob=countsYZ[x][key] / v))
+
+        self.parser = CKYParser(PCFG(N(start), prods))
+
+    def productions(self):
+        """Returns the list of UPCFG probabilistic productions.
+        """
+        return self.prods
+
+    def parse(self, tagged_sent):
+        """Parse a tagged sentence.
+
+        tagged_sent -- the tagged sentence (a list of pairs (word, tag)).
+        """
+        sent, tags = zip(*tagged_sent)
+        prob, tree = self.parser.parse(tags)
+        if prob == float('-inf'):
+            tree = Flat([], 'S').parse(tagged_sent)
+        tree.un_chomsky_normal_form()
+
+        return lexicalize(tree, sent)
